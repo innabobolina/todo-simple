9542994bf73d942cabb16a7a8e6f11723d1211f6
Inna Botchkina <innabobolina@yahoo.com>
2020-10-07 23:48:35 -0700

    add new todo (keypress, detect enter event, grab val from input, append new li) and refactor to use "on" instead of "click"

--- todo.js
+++ todo.js
@@ -1,10 +1,18 @@
 // Cross-out and gray out a specific todo by clicking:
-$("li").click(function () {
+
+$("ul").on("click", "li", function () {
   $(this).toggleClass("completed");
 });
+// on (above) adds listeners for all potential
+// future elements, not just the existing ones,
+// unlike click (below):
+
+// $("li").click(function () {
+//   $(this).toggleClass("completed");
+// });
 
 // Click on X to delete Todo (an li element):
-$("span").click(function (event) {
+$("ul").on("click", "span", function (event) {
   $(this)
     .parent()
     .fadeOut(500, function () {
@@ -13,3 +21,14 @@ $("span").click(function (event) {
   event.stopPropagation(); // prevents bubbling to li, ul, container, body etc
 });
 // fadeOut() doesn't remove but changes to display="hidden";
+
+$("input[type='text']").keypress(function (event) {
+  // event.which === 13 detects when the Enter key is pressed:
+  if (event.which === 13) {
+    // grab new todo text from input:
+    let todoText = $(this).val();
+    $(this).val("");
+    // create a new li and add to ul:
+    $("ul").append(`<li><span>X</span> ${todoText}</li>`);
+  }
+});
