25889e1fd0286e9de2458216301f3ca5f4fdfa11
Inna Botchkina <innabobolina@yahoo.com>
2020-10-07 20:28:35 -0700

    refactor with toggleClass

--- todo.js
+++ todo.js
@@ -1,14 +1,4 @@
+// Cross-out specific todos by clicking:
 $("li").click(function () {
-  if ($(this).css("color") === "rgb(128, 128, 128)") {
-    console.log("it's gray!");
-    $(this).css({
-      color: "black",
-      textDecoration: "none",
-    });
-  } else {
-    $(this).css({
-      color: "gray",
-      textDecoration: "line-through",
-    });
-  }
+  $(this).toggleClass("completed");
 });
