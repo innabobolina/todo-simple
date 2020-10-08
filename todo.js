3523ef8c242cf9bca83c19f015e61f8960a3adb4
Inna Botchkina <innabobolina@yahoo.com>
2020-10-07 23:08:53 -0700

    delete a todo by clicking

--- todo.js
+++ todo.js
@@ -1,4 +1,15 @@
-// Cross-out specific todos by clicking:
+// Cross-out and gray out a specific todo by clicking:
 $("li").click(function () {
   $(this).toggleClass("completed");
 });
+
+// Click on X to delete Todo (an li element):
+$("span").click(function (event) {
+  $(this)
+    .parent()
+    .fadeOut(500, function () {
+      $(this).remove();
+    });
+  event.stopPropagation(); // prevents bubbling to li, ul, container, body etc
+});
+// fadeOut() doesn't remove but changes to display="hidden";
